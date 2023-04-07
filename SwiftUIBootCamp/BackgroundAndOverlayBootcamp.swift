adc0ecbdef29e90533635b6e53de655edfc13f04
sohel-mahmud <sohel7avon@gmail.com>
2023-04-07 16:26:21 +0600

    Frame completed, Overlay ongoing

---
+++ SwiftUIBootCamp/BackgroundAndOverlayBootcamp.swift
@@ -0,0 +1,20 @@
+//
+//  BackgroundAndOverlayBootcamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Macuser on 07/04/2023.
+//
+
+import SwiftUI
+
+struct BackgroundAndOverlayBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BackgroundAndOverlayBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        BackgroundAndOverlayBootcamp()
+    }
+}
--- SwiftUIBootCamp/FramesBootcamp.swift
+++ SwiftUIBootCamp/FramesBootcamp.swift
@@ -10,6 +10,12 @@ import SwiftUI
 struct FramesBootcamp: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .background(Color.green)
+            //.frame(width: 300, height: 300, alignment: .center)
+            .frame(maxWidth: .infinity, alignment: .trailing)
+            .background(Color.red)
+            .frame(height: 100)
+            .background(Color.yellow)
     }
 }
 
