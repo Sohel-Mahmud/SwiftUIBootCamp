f52e1dd7b0da21223b56e880d78bc3e10122b5ac
sohel-mahmud <sohel7avon@gmail.com>
2023-10-17 17:48:32 +0600

    gridview

---
+++ SwiftUIBootCamp/LazyVHGridBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  LazyVHGridBootCamp.swift
+//  SwiftUIBootCamp
+//
+//  Created by Sohel Mahmud on 17/10/23.
+//
+
+import SwiftUI
+
+struct LazyVHGridBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LazyVHGridBootCamp()
+}
